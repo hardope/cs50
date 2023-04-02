08f4f770636bc9638e89b138352477c659385544
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-02 13:22:11 +0100

    Update

--- bank/bank.py
+++ bank/bank.py
@@ -1,14 +1,8 @@
 def main():
-
      greeting = input("Greeting: ").lower()
-
      print(f"${value(greeting)}")
-    
-
 
 def value(greeting):
-     
-
      if "hello" in greeting.lower():
           return 0
 
@@ -17,8 +11,6 @@ def value(greeting):
 
      else:
           return 100
-
-
 
 if __name__ == "__main__":
     main()
